30d6626a3bed5225f7840d1b72321ac15941e901
David Weber <davidweber@Davids-MacBook-Air.local>
2016-08-10 13:45:21 -0700

    Added first if else statement

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,9 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour){
+	alert("You Found a Match!");
+} else {
+	alert("Sorry, try again.")
+}
