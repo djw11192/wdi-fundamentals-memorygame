40ebf342b35b3b0ce7bf153a2e4656baa6b57dbc
David Weber <davidweber@Davids-MacBook-Air.local>
2016-08-10 22:00:53 -0700

    unit 10 work

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,8 +3,22 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour){
+/*if (cardTwo === cardFour){
 	alert("You Found a Match!");
 } else {
 	alert("Sorry, try again.")
+}*/
+
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var getGameBoard = document.getElementById('game-board');
+
+var createBoard = function(){
+
+for(var i = 0; i < cards.length; i++){
+  var newDivs = document.createElement('div');
+  newDivs.className = 'card';
+  getGameBoard.appendChild(newDivs);
 }
+};
+  
